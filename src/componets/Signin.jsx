6c93270a87387337cc375c11b41ebbe59ec25f0c
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-10 12:05:35 +0300

    first commit

--- src/componets/Signin.jsx
+++ src/componets/Signin.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Signin = () => {
+  // Declare hooks here
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState('')
   return (
     <div>
-        <h1>Welcome to the Signin Page</h1>
+     <div className="row justify-content-center mt-4" >
+      <div className="col-md-6 card shadow p-4" >
+      <h1>  Signin Page</h1>
+      <form >
+        <input type="email"
+        placeholder='Enter your email here'
+        required
+        className='form-control' 
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}/> <br />
+        {email}
+        <input type="password"
+        placeholder='Enter your password here'
+        required
+        className='form-control'
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)} /> <br />
+        {password}
+        <button type='submit ' className='btn btn-outline-success'>Submit</button>
+      </form>
+      </div>
+     </div>
         </div>
   )
 }
