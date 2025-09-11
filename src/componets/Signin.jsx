a1f9fbd1cab3e6ad23aaac0a0bd5e39f8a5df236
madlib-doom~ <aaronplimo021@gmail.com>
2025-09-11 12:25:34 +0300

    first commit

--- src/componets/Signin.jsx
+++ src/componets/Signin.jsx
@@ -1,35 +1,115 @@
+import axios from 'axios';
 import React, { useState } from 'react'
+
+import ("./stylings/Signup.css")
+const Loader = () => {
+  return (
+    <div className="section-center">
+      <div className="section-path">
+        <div className="globe">
+          <div className="wrapper">
+            {[...Array(16)].map((_, i) => (
+              <span key={i}></span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Signin = () => {
   // Declare hooks here
   const[email,setEmail]=useState("")
   const[password,setPassword]=useState('')
+
+  
+
+  // create three additional hooks
+  const[loading,setLoading]=useState(false)
+  const [error,setError]=useState("")
+  const [success,setSuccess]=useState('')
+
+  // implement a function to handle login process
+
+  const submit=async (e)=>{
+    e.preventDefault();
+
+    // Update loading hook with a new message
+    setLoading(true)
+
+    try{
+      // creaye a new form data
+      const data=new FormData();
+      // append data
+      data.append("email",email)
+      data.append("password",password)
+//  await for a response from API   endpoint
+      const response=await axios.post("https://aarondev.pythonanywhere.com/api/signin",data)
+// set Loading to false so that it stops loading
+      setLoading(false)
+      console.log(response.data)
+
+      if (response.data.message==="Login succesful"){
+        setSuccess(response.data.message)
+      }
+      else{
+        setError(response.data.message)
+      }
+
+
+    }
+    catch(error){
+      // seloading to false so that it stops loading
+      setLoading(false)
+      setError("An error occured..")
+
+    }
+    
+
+  }
   return (
-    <div>
-     <div className="row justify-content-center mt-4" >
-      <div className="col-md-6 card shadow p-4" >
-      <h1>  Signin Page</h1>
-      <form >
-        <input type="email"
-        placeholder='Enter your email here'
-        required
-        className='form-control' 
-        value={email}
-        onChange={(e)=>setEmail(e.target.value)}/> <br />
-        {email}
-        <input type="password"
-        placeholder='Enter your password here'
-        required
-        className='form-control'
-        value={password}
-        onChange={(e)=>setPassword(e.target.value)} /> <br />
-        {password}
-        <button type='submit ' className='btn btn-outline-success'>Submit</button>
-      </form>
-      </div>
-     </div>
+    <>
+      {loading && <Loader />}
+    
+      {success && <div className="alert alert-success">{success}</div>}
+          {error && <div className="alert alert-danger">{error}</div>}
+  
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-6 card shadow p-4">
+          <h2 className="text-center">Signin Form</h2>
+          <hr />
+  
+          <form onSubmit={submit}>
+            <input
+              type="email"
+              placeholder="Enter your email here"
+              required
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+  
+            <input
+              type="password"
+              placeholder="Enter your password here"
+              required
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+  
+            <button type="submit" className="btn btn-outline-success">
+              Submit
+            </button>
+          </form>
         </div>
-  )
+      </div>
+    </>
+  );
+  
 }
 
 export default Signin
